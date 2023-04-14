054bcb1c1bdc55b95f54aa5932316f5aa38799aa
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-14 14:23:33 +0530

    create model

--- greatcart/settings.py
+++ greatcart/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'category',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATICFILES_DIRS = [
     'greatcart/static',
 
 ]
+
+
