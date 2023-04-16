8f3cb9bffdedb75d64bfa5b933889fc7a0ecb387
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-16 16:40:49 +0530

    calculate tex and grand total

--- carts/views.py
+++ carts/views.py
@@ -47,6 +47,10 @@ def cart(request, total = 0,quantity = 0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+
+        tax = (2 * total)/100
+        grand_total = total + tax 
+
     except ObjectDoesNotExist:
         pass 
 
@@ -54,7 +58,8 @@ def cart(request, total = 0,quantity = 0, cart_items=None):
         'total': total , 
         'quantity': quantity,
         'cart_items':cart_items,
-
+        'tax': tax,
+        'grand_total': grand_total,
     }
 
     return render(request , 'store/cart.html' , context)
