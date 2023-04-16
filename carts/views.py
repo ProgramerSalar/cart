147f2d84ca1abbc6356131c3912165a299fc7e48
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-16 16:01:22 +0530

    cart view for getting cart items , total and quantity

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , redirect
 from store.models import Product
 from carts.models import Cart, CartItem
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -39,5 +40,21 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 
-def cart(request):
-    return render(request , 'store/cart.html')
+def cart(request, total = 0, cart_items = None):
+    try:
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart = cart , is_active = True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass 
+
+    context = {
+        'total': total , 
+        'quantity': quantity,
+        'cart_items':cart_items,
+
+    }
+
+    return render(request , 'store/cart.html' , context)
