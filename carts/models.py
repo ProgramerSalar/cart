21b9f454f720d036199ede0fb682a13bca695ce0
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-16 16:35:49 +0530

    implement data into cart page

--- carts/models.py
+++ carts/models.py
@@ -19,5 +19,9 @@ class CartItem(models.Model):
     is_active = models.BooleanField(default=True)
 
 
+    def sub_total(self):
+        return self.product.price * self.quantity 
+
+
     def __str__(self):
         return self.product
