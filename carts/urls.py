86364ab4682f36622e59b8cf3b186c3cb06ad5a1
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-16 15:48:17 +0530

    Add to Cart functionality without logging-in and with Session Key

--- carts/urls.py
+++ carts/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.cart , name = 'cart')
+    path('', views.cart , name = 'cart'),
+    path('add_cart/<int:product_id>/' , views.add_cart , name = 'add_cart' ),
 
 
 ]
