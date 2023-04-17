14e89dc0fa7e1c359bd6840911324a18bcced3f4
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-17 10:45:35 +0530

    Variation Manager for Variatin Model

--- carts/views.py
+++ carts/views.py
@@ -14,10 +14,10 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    color = request.GET['color']
-    # size = request.GET['size']
-    return HttpResponse(color)
-    exit()
+    if request.method == 'POST':
+        color = request.POST['color']
+        size = request.POST['size']
+        print(color , size)
     
     
     
