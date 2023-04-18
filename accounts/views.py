3764ae97f216f5b428292436deca8ef32585a5f2
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-18 10:35:51 +0530

    User Login Functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
-from django.contrib import messages
+from django.contrib import messages , auth
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
@@ -31,10 +33,24 @@ def register(request):
         
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request,user)
+            # messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
     return render(request , 'accounts/login.html')
 
-
+@login_required(login_url = 'login')
 def logout(request):
-    return render(request , 'accounts/logout.html')
+    auth.logout(request)
+    messages.success(request , 'you are logged out.')
+    return redirect('login')
+    
 
 
