d43cc3c5704bba796a7136615709bacbf0ab586f
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-18 12:08:29 +0530

    Account Activation - Encode User PK and send Token Based Activation Link

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,16 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+# verification the email 
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode , urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+
+
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -21,6 +31,19 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+            
+            # user activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject , message, to=[to_email])
+            send_email.send()
             messages.success(request, 'Registration Successful')
             return redirect('register')
     else:
--- greatcart/settings.py
+++ greatcart/settings.py
@@ -146,3 +146,11 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
     
 }
+
+
+# SMTP configuration 
+EMAIL_HOST = 'smtp.gmail.com',
+EMAIL_PORT = 587 
+EMAIL_HOST_USER = 'manishkumar60708090@gmail.com',
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True 
