4e3147ccdba7740d8ccef608feabe6cb6964bb36
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-19 08:35:07 +0530

    Reset Password Validation

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('deshboard/' , views.deshboard , name = 'deshboard'),
     path('' , views.deshboard , name = 'deshboard'),
     path('forgotPassword/', views.forgotPassword , name = 'forgotPassword'),
+    path('resetPassword/', views.resetPassword , name = 'resetPassword'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
 ]
