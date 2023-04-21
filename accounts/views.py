2f69b6efbed6cd696fad1eb632941a7930bc7d19
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-21 18:32:23 +0530

    Order Detail Page

--- accounts/views.py
+++ accounts/views.py
@@ -22,7 +22,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
 import requests
-from orders.models import Order
+from orders.models import Order , OrderProduct
+
 
 
 def register(request):
@@ -289,3 +290,22 @@ def change_password(request):
             return redirect('change_password')
             
     return render(request , 'accounts/change_password.html')
+
+
+
+
+
+@login_required(login_url='login')
+def order_detail(request , order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number = order_id)
+    order = Order.objects.get(order_number = order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+    context = {
+        'order_detail':order_detail,
+        'order':order,
+        'subtotal':subtotal,
+    }
+    return render(request , 'accounts/order_detail.html',context)
+    
