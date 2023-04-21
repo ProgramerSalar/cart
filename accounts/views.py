80bee8af4ef5cca63029e559312d5497702b3b8b
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-21 18:36:58 +0530

    Fix Profile picture in Deshborad

--- accounts/views.py
+++ accounts/views.py
@@ -161,8 +161,10 @@ def activate(request, uidb64 , token):
 def deshboard(request):
     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
     orders_count = orders.count()
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
     context = {
-        'orders_count':orders_count
+        'orders_count':orders_count,
+        'userprofile':userprofile,
     }
     return render(request , 'accounts/deshboard.html', context)
     
