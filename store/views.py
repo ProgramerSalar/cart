c7f2a4141424f1655961ba12d8a9ffaf5e3b559e
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-21 11:20:56 +0530

    Displaying Rating Stars

--- store/views.py
+++ store/views.py
@@ -55,11 +55,13 @@ def product_detail(request , category_slug , product_slug):
     except OrderProduct.DoesNotExist:
         orderproduct = None    
    
-    
+    # get the reviews 
+    reviews = ReviewRating.objects.filter(product_id=single_product.id ,status = True)
     context = {
         'single_product': single_product,
         'in_cart':in_cart,
         'orderproduct':orderproduct,
+        'reviews':reviews
     }
     
     return render(request , 'store/product_detail.html' ,context)
